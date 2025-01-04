466f9392f50360a965ffbb780ad8583a4b3ff739
Fbshie <rivatyas94@gmail.com>
2025-01-04 21:18:09 +0700

    1

--- src/app/admin/editKamar/[id]/page.tsx
+++ src/app/admin/editKamar/[id]/page.tsx
@@ -1,43 +1,32 @@
-import { GetServerSideProps } from "next";
 import NavbarAdmin from "../../adminComponents/NavbarAdmin";
 import EditKamarForm from "../../editComponents/EditKamarForm";
 
-const getKamarById = async (id: string): Promise<{ kamar: { jumlah: number } }> => {
+const getKamarById = async (id: string) => {
     try {
         const res = await fetch(`http://localhost:3000/api/kamar/${id}`, {
             cache: "no-store",
         });
 
         if (!res.ok) {
-            throw new Error("Failed to fetch data Kamar");
+            throw new Error("Failed to fetch data Kamar")
         }
 
-        const data = await res.json();
-        return data;
+        return res.json();
     } catch (error) {
-        console.error(error);
-        return { kamar: { jumlah: 0 } };
+        console.log(error);
     }
 };
 
-export default function EditKamarPage({ id, jumlah }: { id: string; jumlah: number }) {
+export default async function editKamar({ params }: { params: { id?: string } }) {
+    const id = params.id ?? 'default-id';
+    const { kamar } = await getKamarById(id);
+    const { jumlah } = kamar;
+
     return (
         <>
+
             <NavbarAdmin />
             <EditKamarForm id={id} jumlah={jumlah} />
         </>
     );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { id } = context.params as { id: string };
-
-    const { kamar } = await getKamarById(id);
-
-    return {
-        props: {
-            id,
-            jumlah: kamar.jumlah,
-        },
-    };
-};
