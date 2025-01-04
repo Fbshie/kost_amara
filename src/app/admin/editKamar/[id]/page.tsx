86ec0b6606a98cbd3994730036c37dce2fc2824b
Fbshie <rivatyas94@gmail.com>
2025-01-04 18:47:12 +0700

    v6

--- src/app/admin/editKamar/[id]/page.tsx
+++ src/app/admin/editKamar/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { GetServerSidePropsContext } from "next";
 import NavbarAdmin from "../../adminComponents/NavbarAdmin";
 import EditKamarForm from "../../editComponents/EditKamarForm";
 
@@ -11,15 +12,16 @@ const getKamarById = async (id: string): Promise<{ kamar: { jumlah: number } }> 
             throw new Error("Failed to fetch data Kamar")
         }
 
-        return res.json();
+        const data = await res.json();
+        return data;
     } catch (error) {
         console.log(error);
         return { kamar: { jumlah: 0 } }; 
     }
 };
 
-export default async function editKamar({ params }: { params: { id: string } }) {
-    const id = params.id ?? 'default-id';
+export default async function editKamar({ params }: GetServerSidePropsContext< { id: string }>) {
+    const id = params?.id || "default-id";
     const { kamar } = await getKamarById(id);
     const { jumlah } = kamar;
 
