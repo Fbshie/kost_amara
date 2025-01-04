54e5bda7e99219b24299ce6a1d4f7b5942579da6
Fbshie <rivatyas94@gmail.com>
2025-01-04 20:46:31 +0700

    v9

--- src/app/admin/editKamar/[id]/page.tsx
+++ src/app/admin/editKamar/[id]/page.tsx
@@ -1,31 +1,33 @@
+import { GetServerSidePropsContext } from "next";
 import NavbarAdmin from "../../adminComponents/NavbarAdmin";
 import EditKamarForm from "../../editComponents/EditKamarForm";
 
 const getKamarById = async (id: string): Promise<{ kamar: { jumlah: number } }> => {
-
     try {
         const res = await fetch(`http://localhost:3000/api/kamar/${id}`, {
             cache: "no-store",
         });
 
         if (!res.ok) {
-            throw new Error("Failed to fetch data Kamar");
+            throw new Error("Failed to fetch data Kamar")
         }
 
-        return await res.json();
+        const data = await res.json();
+        return data;
     } catch (error) {
-        console.error("Error fetching kamar:", error);
+        console.log(error);
         return { kamar: { jumlah: 0 } }; 
     }
 };
 
-export default async function EditKamarPage({ params }: { params: { id: string } }) {
-    const { id } = params;
+export default async function editKamar({ params }: GetServerSidePropsContext< { id: string }>) {
+    const id = params?.id || "default-id";
     const { kamar } = await getKamarById(id);
     const { jumlah } = kamar;
 
     return (
         <>
+
             <NavbarAdmin />
             <EditKamarForm id={id} jumlah={jumlah} />
         </>
