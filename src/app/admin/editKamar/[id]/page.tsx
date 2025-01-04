0c16aaa589ac4c08f3d9b826022e7fca98ba1fc2
Fbshie <rivatyas94@gmail.com>
2025-01-04 19:50:17 +0700

    v8

--- src/app/admin/editKamar/[id]/page.tsx
+++ src/app/admin/editKamar/[id]/page.tsx
@@ -2,31 +2,30 @@ import NavbarAdmin from "../../adminComponents/NavbarAdmin";
 import EditKamarForm from "../../editComponents/EditKamarForm";
 
 const getKamarById = async (id: string): Promise<{ kamar: { jumlah: number } }> => {
+
     try {
         const res = await fetch(`http://localhost:3000/api/kamar/${id}`, {
             cache: "no-store",
         });
 
         if (!res.ok) {
-            throw new Error("Failed to fetch data Kamar")
+            throw new Error("Failed to fetch data Kamar");
         }
 
-        const data = await res.json();
-        return data;
+        return await res.json();
     } catch (error) {
-        console.error(error);
+        console.error("Error fetching kamar:", error);
         return { kamar: { jumlah: 0 } }; 
     }
 };
 
-export default async function editKamar({ params }: { params: { id: string } }) {
-    const id = params.id;
+export default async function EditKamarPage({ params }: { params: { id: string } }) {
+    const { id } = params;
     const { kamar } = await getKamarById(id);
     const { jumlah } = kamar;
 
     return (
         <>
-
             <NavbarAdmin />
             <EditKamarForm id={id} jumlah={jumlah} />
         </>
