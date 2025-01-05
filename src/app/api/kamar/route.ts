5311463d67af5228145e6862664e07d4853b4e1e
Fbshie <rivatyas94@gmail.com>
2025-01-05 07:03:47 +0700

    Update route.ts

--- src/app/api/kamar/route.ts
+++ src/app/api/kamar/route.ts
@@ -2,24 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import connectMongoDB from "../../../../libs/mongodb";
 import Kamar from "../../../../models/KamarModel";
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+export async function GET() {
     await connectMongoDB();
-
-    const { id } = params;
-    if (id) {
-        const kamar = await Kamar.findById(id);
-        if (!kamar) {
-            return NextResponse.json({ message: "Data tidak ditemukan" }, { status: 404 });
-        }
-        return NextResponse.json({ kamar });
-    }
-
     const kamar = await Kamar.find();
     return NextResponse.json({ kamar });
 }
 
-export async function POST(request: NextRequest) {
-    const { jumlah } = await request.json();
+export async function POST(request: Request) {
+    const {jumlah} = await request.json();
     await connectMongoDB();
     await Kamar.create({ jumlah });
     return NextResponse.json({ message: "Data ditambahkan" }, { status: 201 });
@@ -27,15 +17,7 @@ export async function POST(request: NextRequest) {
 
 export async function DELETE(request: NextRequest) {
     const id = request.nextUrl.searchParams.get("id");
-    if (!id) {
-        return NextResponse.json({ message: "ID tidak diberikan" }, { status: 400 });
-    }
-
     await connectMongoDB();
-    const kamar = await Kamar.findByIdAndDelete(id);
-    if (!kamar) {
-        return NextResponse.json({ message: "Data tidak ditemukan" }, { status: 404 });
-    }
-
-    return NextResponse.json({ message: "Data dihapus" }, { status: 200 });
+    await Kamar.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Data dihapus" }, {status:200});
 }
