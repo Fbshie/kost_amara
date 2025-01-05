8224ac910cc5603e017cf6ee73e02a248f6b3e37
Fbshie <rivatyas94@gmail.com>
2025-01-05 14:12:38 +0700

    v22

--- src/app/editKamar/[id]/page.tsx
+++ src/app/editKamar/[id]/page.tsx
@@ -1,8 +1,6 @@
 import NavbarAdmin from "@/app/admin/adminComponents/NavbarAdmin";
 import EditKamarForm from "@/app/admin/editComponents/EditKamarForm";
 
-
-
 const getKamarById = async (id: string) => {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/kamar/${id}`, {
@@ -10,24 +8,38 @@ const getKamarById = async (id: string) => {
         });
 
         if (!res.ok) {
-            throw new Error("Failed to fetch data Kamar")
+            throw new Error("Failed to fetch data Kamar");
         }
 
-        
         return res.json();
     } catch (error) {
-        console.log(error);
+        console.error("Error fetching data:", error);
+        return null;
     }
 };
 
-export default async function editKamar({ params }: { params: { id?: string } }) {
-    const id = params.id ?? 'default-id';
-    const { kamar } = await getKamarById(id);
+interface EditKamarProps {
+    params: { id: string };
+}
+
+export default async function editKamar({ params }: EditKamarProps) {
+    const id = params.id || "default-id";
+    const data = await getKamarById(id);
+
+    if (!data) {
+        return (
+            <>
+                <NavbarAdmin />
+                <p>Failed to load data for the selected room.</p>
+            </>
+        );
+    }
+
+    const { kamar } = data;
     const { jumlah } = kamar;
 
     return (
         <>
-
             <NavbarAdmin />
             <EditKamarForm id={id} jumlah={jumlah} />
         </>
